d19fb257495f854ab7d072fcbf915b6cf91cfeac
toanl33 <toan.le3370@gmail.com>
2021-10-17 06:34:36 +0700

    Working on edit functions.

--- src/components/user/post/recipe/RecipeStep03.js
+++ src/components/user/post/recipe/RecipeStep03.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link, useHistory} from "react-router-dom";
 import {FaAngleLeft} from "react-icons/fa";
 
@@ -7,18 +7,36 @@ const RecipeStep03 = (props) => {
     const history = useHistory();
     const [ingredient, setIngredient] = useState({ingredient_name: "", amount_in_mg: "0"});
 
-    const handleChange = (field) => (event) => {
-        setIngredient({...ingredient, [field]: event.target.value});
+    const handleAddItem = (e) => {
+        e.preventDefault();
+        const ingredient = {
+            ingredient_name: "",
+            amount_in_mg: "0",
+        }
+        props.setIngredients((prev) => [...prev, ingredient]);
     }
-    const addIngredient = (event) => {
-        event.preventDefault();
-        props.setIngredients(props.ingredients.concat(ingredient));
-        setIngredient({ingredient_name: "", amount_in_mg: "0"});
+    const handleChange = (index, e) => {
+        e.preventDefault();
+        e.persist();
+        props.setIngredients((prev) => {
+            return prev.map((item, i) => {
+                if (i !== index) {
+                    return item;
+                }
+                return {
+                    ...item, [e.target.name]: e.target.value,
+                };
+            });
+        });
     }
+
 
     const nextStep = () => {
         history.push("/post/recipe/step-4");
     }
+    useEffect(() => {
+        console.log(props.ingredients)
+    }, [props.ingredients])
 
     return (
         <>
@@ -30,47 +48,28 @@ const RecipeStep03 = (props) => {
                         estimate the nutritional values for your recipe better.</em>
                 </header>
                 <div className="section-content">
-                    <form className="form-full" onSubmit={addIngredient}>
+                    <form className="form-full">
                         <h1>Name an ingredient and its amount (in grams)</h1>
-                        <div className="flex-horizontal">
-                            <input aria-label="Ingredient" type="text" value={ingredient.ingredient_name}
-                                   onChange={handleChange("ingredient_name")}
-                                   placeholder="e.g: lettuce, tomato, basil,..." required/>
-                            <input aria-label="Amount" type="number" value={ingredient.amount_in_mg}
-                                   onChange={handleChange("amount_in_mg")}/>
-                        </div>
-                        <button type="submit">Add ingredient</button>
-                    </form>
-                </div>
-            </section>
-            <section>
-                <header className="section-header">
-                    <h1>Your ingredients</h1>
-                </header>
-                <div className="section-content">
-                    <form className="form-full" onSubmit={nextStep}>
                         {props.ingredients.length > 0 ?
-                            <>
-                                <ul className="ingredient-list">
-                                    {props.ingredients.map(ingredient => (
-                                        <li className="flex-horizontal" key={ingredient.ingredient_name}>
-                                            <input aria-label="Ingredient" type="text"
-                                                   value={ingredient.ingredient_name}
-                                                   onChange={handleChange("name")}
-                                                   placeholder="e.g: lettuce, tomato, basil,..." required/>
-                                            <input aria-label="Amount" type="number" value={ingredient.amount_in_mg}
-                                                   onChange={handleChange("amount")}/>
-                                        </li>
-                                    ))}
-                                </ul>
-                                <button type="submit">Next step</button>
-                            </>
+                            <ul className="ingredient-list">
+                                {props.ingredients.map((item, index) => (
+                                    <li className="flex-horizontal" key={index}>
+                                        <input name="ingredient_name" type="text"
+                                               value={item.ingredient_name}
+                                               onChange={(e) => handleChange(index, e)}
+                                               placeholder="e.g: lettuce, tomato, basil,..." required/>
+                                        <input name="amount_in_mg" type="number"
+                                               value={item.amount_in_mg}
+                                               onChange={(e) => handleChange(index, e)}/>
+                                    </li>
+                                ))}
+                            </ul>
                             :
-                            <>
-                                <em>Add something to proceed with your recipe...</em>
-                                <button type="submit" disabled>Next step</button>
-                            </>
-                        }
+                            <em>Add some ingredients to your recipe...</em>}
+                        <div className="flex-horizontal">
+                            <button onClick={handleAddItem}>Add ingredient</button>
+                            <button onClick={nextStep} type="submit">Next step</button>
+                        </div>
                     </form>
                 </div>
             </section>
