229365ab540864ad3881fbde192bf8b7febcfa2b
toanl33 <toan.le3370@gmail.com>
2021-11-01 21:50:16 +0700

    Caught exceptions in Home banner.

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -1,5 +1,5 @@
 // export const apiBase = "https://vegetarianapplication.herokuapp.com/api"
 
-// export const apiBase = "http://14.161.47.36:8080/hiepphat-0.0.1-SNAPSHOT/api"
+export const apiBase = "http://14.161.47.36:8080/hiepphat-0.0.1-SNAPSHOT/api"
 
-export const apiBase = "https://vegetarian-api.herokuapp.com/api"
+// export const apiBase = "https://vegetarian-api.herokuapp.com/api"
